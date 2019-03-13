ffd1277fef99ded0582165d69057c0b9523476f2
KadekJ <46430136+KadekJ@users.noreply.github.com>
2019-03-13 13:34:00 +0700

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,22 @@
+<?php
+
+    include '../connect.php';
+
+    $id_dosen = $_GET['id_dosen'];
+
+    $query = "DELETE FROM dosen WHERE id_dosen = $id_dosen";
+
+    $result = mysqli_query($connect, $query);
+
+    $num = mysqli_affected_rows($connect);
+     
+     if ($num > 0)
+     {
+         echo "Gagal hapus data<br>";
+     } else 
+     {
+         echo "Berhasil hapus data <br>";
+     }
+
+     echo "<a href='read.php'>Lihat Data</a>";
+?>
---
+++ form-create.php
@@ -0,0 +1,9 @@
+<h3>Tambah Data Dosen</h3>
+    <form action="create.php" method="POST">
+        Nama Dosen : <input type="text" name="nama_dosen"><br>
+        <br>
+        No.Telepon : <input type="text" name="telp"><br>
+        <br>
+        <input type="submit" value="Simpan" name="btnSimpan">
+    </form>
+    
